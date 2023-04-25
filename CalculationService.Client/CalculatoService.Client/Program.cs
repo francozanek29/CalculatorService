9de1822882e618b4cef0bbeb409323ae22af3afa
Franco Zanek <zanekfranco@gmail.com>
2023-04-25 22:27:52 +0200

    Add client project

--- CalculationService.Client/CalculatoService.Client/Program.cs
+++ CalculationService.Client/CalculatoService.Client/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool showMenu = true;
+while (showMenu)
+{
+  Console.WriteLine("1. Opción 1");
+  Console.WriteLine("2. Opción 2");
+  Console.WriteLine("3. Salir");
+  Console.Write("Elija una opción: ");
+  var option = Console.ReadLine();
+
+  switch (option)
+  {
+    case "1":
+      // llamar a una función para manejar la opción 1
+      break;
+    case "2":
+      // llamar a una función para manejar la opción 2
+      break;
+    case "3":
+      showMenu = false;
+      break;
+    default:
+      Console.WriteLine("Opción inválida");
+      break;
+  }
+  Console.Clear();
+}
--- CalculatorService.Server/src/CalculatorService.Server.WebAPI/Models/AddOperationModel.cs
+++ CalculatorService.Server/src/CalculatorService.Server.WebAPI/Models/AddOperationModel.cs
@@ -1,16 +1,18 @@
 ﻿using CalculatorService.Server.WebAPI.Validations;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CalculatorService.Server.WebAPI.Models
 {
-    /// <summary>
-    /// Record in charge of handling the information for a Add operation parameters in the controller. 
-    /// It receives the data from the user and send the data to the application and viceversa. 
-    /// </summary>
-    public record AddOperationModel
-    {
-        [Required]
-        [EnsureMinimumElementsAttribute(2, ErrorMessage = "At least two elements should be provided")]
-        public IEnumerable<int> Addends { get; set; } = new int[0];
-    }
+  /// <summary>
+  /// Record in charge of handling the information for a Add operation parameters in the controller. 
+  /// It receives the data from the user and send the data to the application and viceversa. 
+  /// </summary>
+  public record AddOperationModel
+  {
+    [Required]
+    [JsonPropertyName("Addends")]
+    [EnsureMinimumElementsAttribute(2, ErrorMessage = "At least two elements should be provided")]
+    public IEnumerable<int> Addends { get; set; } = new List<int>();
+  }
 }
